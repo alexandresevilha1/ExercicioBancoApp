c81e753c2fea13d826635ac611e7e169ea8f7f6a
A l e x a n d r e <A l e x a n d r e@DESKTOP-URJU1R1>
2023-06-26 21:54:33 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioBancoApp/Conta.cs
@@ -0,0 +1,64 @@
+﻿namespace ExercicioBancoApp
+{
+    public class Conta
+    {
+        private int _numeroConta;
+        private string _titular;
+        public double Saldo { get; private set; }
+
+        public Conta(int numeroConta, string titular)
+        {
+            _numeroConta = numeroConta;
+            _titular = titular;
+        }
+
+        public Conta(int numeroConta, string titular, double depositoInicial) : this(numeroConta, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public int NumeroConta
+        {
+            get { return _numeroConta; }
+            set
+            {
+                if ( value > 1)
+                {
+                    _numeroConta = value;
+                }
+            }
+        }
+
+        public string Titular 
+        {
+            get { return _titular; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                { _titular = value; }
+
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+        }
+
+        public void Saque(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo -= valor + 5;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + _numeroConta + ", Titular: " + _titular + ", Saldo: $" + Saldo;
+        }
+    }
+}
---
+++ ExercicioBancoApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using ExercicioBancoApp;
+using System.Drawing;
+
+Conta x;
+
+Console.Write("Entre o número da conta:");
+int numeroConta = int.Parse(Console.ReadLine());
+
+Console.Write("Entre o titular da conta:");
+string titular = Console.ReadLine();
+
+Console.Write("Haverá um depósito inicial (s/n)?");
+string opcao = Console.ReadLine();
+
+if (opcao == "s")
+{
+    Console.Write("Entre o valor de depósito inicial:");
+    double valor = int.Parse(Console.ReadLine());
+
+    x = new Conta(numeroConta, titular, valor);
+    Console.WriteLine(x);
+
+    Console.Write("Entre um valor para depósito: ");
+    double deposito = double.Parse(Console.ReadLine());
+    x.Deposito(deposito);
+    Console.WriteLine("Dados da conta atualizados:");
+    Console.WriteLine(x);
+
+    Console.Write("Entre um valor para saque: ");
+    double saque = double.Parse(Console.ReadLine());
+    x.Saque(saque);
+    Console.WriteLine("Dados da conta atualizados:");
+    Console.WriteLine(x);
+}
+else if (opcao == "n")
+{
+    x = new Conta(numeroConta, titular);
+    Console.WriteLine(x);
+
+    Console.Write("Entre um valor para depósito: ");
+    double deposito = double.Parse(Console.ReadLine());
+    x.Deposito(deposito);
+
+    Console.WriteLine("Dados da conta atualizados:");
+    Console.WriteLine(x);
+
+    Console.Write("Entre um valor para saque: ");
+    double saque = double.Parse(Console.ReadLine());
+    x.Saque(saque);
+
+    Console.WriteLine("Dados da conta atualizados:");
+    Console.WriteLine(x);
+}
+
